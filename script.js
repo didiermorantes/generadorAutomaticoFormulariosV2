8d580c4c0efe773ea1424caf38612e3061d426a1
didiermorantes <didiermorantes@gmail.com>
2019-09-10 14:29:12 -0500

    Lista con 3 eventos, nombres de campos y pintando solo los editable

--- script.js
+++ script.js
@@ -520,6 +520,260 @@ var datoLeido=0;
     
      }
 
+
+
+  
+ 
+ /* funcion que genera cajas de acuerdo a la respuesta json de la BD y a la lista seleccionada*/
+ function leerBDListaEspecificos(){
+
+    
+    var datoSeleccionadoLista10 = document.getElementById("miListaEventosEspecificos").value;
+    
+    document.getElementById("demo2").innerHTML = "Eligio Tabla Evento : " + datoSeleccionadoLista10;
+
+
+    var datoLeido14=0;
+    var datoLeido15=0;//variable para contabilizar la cantidad de elementos en el objeto de la
+     //fetch toma como argumento la ruta del recurso que quiere obtener
+     //fetch('json/personas.json')  --DESCOMENTARIAR SI SE USA EL JSON PERSONAS
+     //fetch('http://localhost:8181/api_db/tipoEvento/leer.php') --DESCOMENTARIAR SI SE USA XAMPP CON PUERTO 8181
+
+     var myJsonProcesado10 =0; //variable para guardar el json procesado
+     var myJsonProcesado12 =0; //variable para guardar el json procesado
+     var datoLeidoJsonProcesado10 =0;  //Variable para guardar la seleccion de la lista desplegable
+     var idMyJsonProcesado10=0;//Variable para guardar el id del primer valor del jsonProcesado
+     var idMyJsonProcesado12=0;//Variable para guardar el id del ultimo valor del jsonProcesado
+     
+     
+     fetch('https://vitalsaludem.com/services/api_db/tipoEvento/leerLista.php')
+     .then(function(response14) {
+         //devuelve un objeto promise conteniendo la respuesta, el objeto response
+             //response es una respuesta http y no el archivo json, por tanto, usamos el método json() para extraer el contenido
+         return response14.json();
+     })
+     .then(function(myJson14) {
+       
+         //contamos la cantidad de elementos que tiene el json en el objeto "registros" y lo guardamos en datoLeido
+         //si no especificamos el objeto, el json solamente tiene un elemento, es decir, solamente tiene el objeto registros
+       //datoLeido=Object.keys(myJson2.personas).length ;  --DESCOMENTARIAR SI SE USA EL JSON PERSONAS
+    
+            //contamos la cantidad de elementos que tiene el json en el objeto "registros" y lo guardamos en datoLeido
+         //si no especificamos el objeto, el json solamente tiene un elemento, es decir, solamente tiene el objeto registros
+       datoLeido14=Object.keys(myJson14.registros).length; //esto arroja la cantidad de registros TOTAL que tiene el json
+       
+
+         //luego de obtener la respuesta  con el contenido del json, lo mostramos en consola. Podemos mostrarlo sin ningún tratamiento ya que la consola permite ver objetos
+         console.log(myJson14);
+      // console.log('La cantidad de elementos que tiene el json en el objeto "personas" es : '+datoLeido);    ---DESCOMENTARIAR SI SE USA EL JSON PERSONAS
+       console.log('La cantidad de elementos que tiene el json en el objeto "registros" es : '+datoLeido14);
+       //stringify convierte el objeto en json para ser visualizado por el alert, o si no  solo se observa object object
+       alert(JSON.stringify(myJson14));
+     
+       /* PROCESO PARA DETERMINAR LA TABLA DESEADA , DE ACUERDO A LA LISTA DESPLEGABLE */
+                    myJsonProcesado10 =myJson14.registros[datoSeleccionadoLista10]; 
+
+                    var testigoPrimerDato10=0;//variable para encontrar el primer dato que tenga el idTablaEvento
+                    var testigoUltimoDato10=0;//Variable para encontrar el ultimo dato que tenga el idTablaEvento
+                    //ciclo para encontrar los id de los elementos que coinciden con lo seleccionado en la lista desplegable   
+                    for(var p14 = 0; p14 < datoLeido14; p14++){
+                       
+                          if(myJson14.registros[p14].idTablaEvento == datoSeleccionadoLista10){
+                                //Validamos que sea el primer dato que se encuentra para no usar break
+                                if(testigoPrimerDato10 == 0){
+
+                                    //obtenemos el id del registro que coincide con lo seleccionado en la lista
+                                    idMyJsonProcesado10=myJson14.registros[p14].id;
+                                    //encontramos el indice de dicho id
+                                    myJsonProcesado10 = p14;
+                                    
+                                    console.log("Este es el id del primer dato que coincide con lo seleccionado en la lista: "+idMyJsonProcesado10);
+                                    console.log("Este es el indice del ciclo que coincide con lo seleccionado en la lista "+myJsonProcesado10);
+                                    //cambiamos el valor del testigo para que nunca vuelva a entrar aqui
+                                    testigoPrimerDato10=1;
+                                }//cierre if
+
+                                            
+                            
+                          }//cierre if
+
+                          //validamos si el idTablaEvento es superior a lo seleccionado en la lista
+                          //como la consulta está ordenada de manera ascendente, si el idTablaEvento es superior al seleccionado significa que llegamos al ultimo valor
+                          //es decir, ya estamos en un nuevo idTablaEvento
+                          if(myJson14.registros[p14].idTablaEvento > datoSeleccionadoLista10){
+                            //Validamos que sea el primer dato que se encuentra para no usar break
+                            if(testigoUltimoDato10 == 0){
+
+                                //obtenemos el id del registro que coincide con lo seleccionado en la lista
+                                idMyJsonProcesado12=myJson14.registros[p14].id;
+                                //encontramos el indice de dicho id
+                                myJsonProcesado12 = p14;
+                                
+                                console.log("Este es el id del ultimo dato que coincide con lo seleccionado en la lista (ya no es de la tabla seleccionada): "+idMyJsonProcesado12);
+                                console.log("Este es el indice del ciclo que coincide con lo seleccionado en la lista "+myJsonProcesado12);
+                                //cambiamos el valor del testigo para que nunca vuelva a entrar aqui
+                                testigoUltimoDato10=1;
+                            }//cierre if
+
+                                        
+                        
+                      }//cierre if
+                          
+                        }//cierre for
+                      
+
+
+                    //luego de obtener la respuesta  con el contenido del json processado, lo mostramos en consola. Podemos mostrarlo sin ningún tratamiento ya que la consola permite ver objetos
+                    
+                    
+                            
+                    datoLeidoJsonProcesado10 = Object.keys(myJson14.registros[myJsonProcesado10]).length;
+                            
+                    console.log('La cantidad de elementos que tiene el json procesado en el objeto "registros" en la posicion '+datoSeleccionadoLista10+' es : '+datoLeidoJsonProcesado10);
+                    
+                           //stringify convierte el objeto en json para ser visualizado por el alert, o si no  solo se observa object object
+                     alert(JSON.stringify(myJson14.registros[myJsonProcesado10]));
+                     alert('La cantidad de elementos que tiene el json procesado en el objeto "registros" en la posicion  '+datoSeleccionadoLista10+' es : '+datoLeidoJsonProcesado10);
+
+                     //tenemos que extraer el id del primer elemento de la tabla seleccionada para comenzar a pintar desde ahi. El ordenamiento en la consulta cambia todo
+                    
+                     console.log('El id del primer valor obtenido de la tabla segun lista desplegable es  '+myJsonProcesado10);
+                     alert('El id del primer valor obtenido de la tabla segun lista desplegable es  '+idMyJsonProcesado10);
+
+                     console.log('El id del ultimo valor obtenido de la tabla segun lista desplegable es  '+myJsonProcesado12);
+                     alert('El id del ultimo valor obtenido de la tabla segun lista desplegable es  '+idMyJsonProcesado12);
+
+
+
+
+
+        // crea un nuevo div 
+        // y añade contenido 
+        var newParentDiv14 = document.createElement("div"); 
+        newParentDiv14.setAttribute('class', 'd-inline-flex flex-wrap');
+        // añade el elemento creado y su contenido al DOM 
+        var currentDiv14 = document.getElementById("div"); 
+        document.body.insertBefore(newParentDiv14, currentDiv14); 
+        //el ciclo creará cajas hasta el límite determinado por el datoLeido
+        //el for cuando se trabaja con json si comienza en cero, a diferencia del otro dato que se ingresaba por teclado, donde debia comenzar en uno, ademas la comparación ya no es menor o igual, es solo menor
+               
+       // for(var i4=0; i4<datoLeido4;i4++){ //DESCOMENTARIAR SI SE QUIEREN PINTAR TODOS LOS CAJONES i=O
+
+
+        //LOS LIMITES DE ESTE CICLO SON EL idTablaEvento y el lenght obtenido  de ese registro
+                        var limiteInferior10 = parseInt(myJsonProcesado10);
+                        console.log('El limite inferior del ciclo es '+limiteInferior10);
+                        var limiteSuperior10 =  parseInt(myJsonProcesado12)
+                        console.log('El limite superior del ciclo es '+limiteSuperior10);
+
+
+
+                        console.log('El primer elemento del objeto completo segun el id de la lista es: ');
+                        console.log(myJson14.registros[limiteInferior10]);
+                        console.log('El ultimo elemento del objeto completo segun el id de la lista es: ');
+                        console.log(myJson14.registros[limiteSuperior10]);
+
+//no lo hacemos menor o igual al limite superior, pues el limite superior ya es parte de otra tabla
+                    for(var i14=limiteInferior10; i14<limiteSuperior10;i14++){
+
+//ANTES DE CREAR EL DIV VERIFICAMOS SI EL CAMPO ES EDITABLE PARA VISUALIZARLO O NO
+
+let testigo=myJson14.registros[i14].editable;
+let nombreTestigo = myJson14.registros[i14].descripcion;
+//al parecer el testigo retorna un caracter vacio (espacio) cuando es falso
+//evaluamos por el caracter Unicode ya que otras validaciones fallaron
+//validamos la posición(0) de testigo (El primer  bit)
+if(testigo.charCodeAt(0) == 0){
+
+    //no hacemos nada, y por tanto no se pintará
+console.log('El valor del campo editable  para '+nombreTestigo+ ' es: ' + testigo);
+}
+else
+{
+    console.log('El tipo de datos del testigo es '+testigo.charCodeAt(0));
+    console.log('El valor del campo editable  para '+nombreTestigo+ ' es: ' + testigo);
+      // crea un nuevo div 
+                // y añade contenido 
+                var newDiv14 = document.createElement("div"); 
+                //var newContent2 = document.createTextNode("Campo: "+i2);  --DESCOMENTARIAR SI SE QUIERE CONTENEDOR GENERICO
+                
+               // alert(myJson2.campos[i2].nombreCampo);      --DESCOMENTARIAR SI SE QUIERE VER CADA NOMBRE DE CAMPO EN UN ALERT
+               //asignamos el nombre del campo de acuerdo al contenido del json
+                var newContent14 = document.createTextNode(myJson14.registros[i14].descripcion); 
+            
+                
+                newDiv14.appendChild(newContent14); //añade texto al div creado.  
+                // añade el elemento creado y su contenido al DOM 
+                var currentDiv14 = document.getElementById("div"); 
+                document.body.insertBefore(newDiv14, currentDiv14); 
+    
+    
+            //creamos elemento de tipo input
+                var input14 = document.createElement("input");     
+                //establecemos el tipo texto
+                //input2.setAttribute('type', 'text'); --DESCOMENTARIAR SI SE QUIERE UN CONTENEDOR GENERICO DE TIPO TEXT
+    
+                //asignamos el tipo de dato, de acuerdo al json
+                input14.setAttribute('type', myJson14.registros[i14].tipo);
+                //input2.name = "caja"+i2; --DESCOMENTARIRAR SI SE QUIERE UN NAME GENERICO
+                //input2.id="caja"+i2;   --DESCOMENTARIAR SI SE QUIERE UN ID GENERICO
+    
+                    //asignamos como name e id, la información del nombre del campo proveniente del json
+                input14.name = myJson14.registros[i14].nombre;
+                input14.id=myJson14.registros[i14].nombre;
+    
+                //campos en linea
+                //input.setAttribute('class', 'd-sm-inline-flex');
+                input14.setAttribute('class', 'd-sm-flex');
+                //le damos bordes al div
+                    //insertamos función javascript al elemento con el evento onblur, es decir, cuando pierde el foco
+                    input14.setAttribute('onblur',myJson14.registros[i14].jsEvento);
+    
+    
+                newDiv14.setAttribute('style','border: thin solid black');
+                //asignamos un color a los div pares y otro a los impares
+                    if(i14%2==0){
+                        //color azul turquesa a los pares
+                        newDiv14.setAttribute('class','p-2 bg-light');
+                    }
+                    else{
+                        //color amarillo a los impares
+                        newDiv14.setAttribute('class','p-2 bg-secondary');
+                    }
+    
+                newDiv14.appendChild(input14); //añade la caja de texto al div creado recientemente. 
+            //se crearán tantos div como el número que se haya ingresado en la caja de texto
+            newParentDiv14.appendChild(newDiv14); //añadimos cada div a su padre
+
+
+}//fin else
+
+          
+        }//fin for
+    
+    
+    
+    
+     });
+    
+    
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       /* funcion que genera cajas de acuerdo a la respuesta json de la BD y a la lista seleccionada*/
  function leerBDCaja(){
 
