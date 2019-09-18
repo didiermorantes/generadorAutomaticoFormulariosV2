e79d3e41037bb94d2e3ca8db17603c31357b99ac
didiermorantes <didiermorantes@gmail.com>
2019-09-18 17:05:28 -0500

    alertas en la caja tipo num de acuerdo a cantidad de elementos del json y lista desplegable cargada

--- funcionesValidaCampos.js
+++ funcionesValidaCampos.js
@@ -16,42 +16,63 @@ function validaTipoCancer(){
         miTipoCancer.value='Diligencie datos Válidos';
     }//fin if
 }//fin validaTipoCancer
+/*
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+*/
 function poblarListaTipoCancerMenor18(){
 
 
     if (typeof(Storage) !== "undefined") {
         // Code for localStorage/sessionStorage.
 
-
-        alert("Funciona la creación dinámica del js");
         var miListaTipoCancerMenor18 = document.getElementById('TIPO_CA');
-        alert("El valor seleccionado fue: "+miListaTipoCancerMenor18.value);
+     
+
 //recuperamos el total de listas deplegables.
 // Retrieve
 var totalListasDesplegablesProcesar = localStorage.totalListasDesplegables ;
-alert("La cantidad de listas deplegables en este formulario es : "+totalListasDesplegablesProcesar);
-
+//alert("La cantidad de listas deplegables en este formulario es : "+totalListasDesplegablesProcesar);
+console.log("La cantidad de listas deplegables en este formulario es : "+totalListasDesplegablesProcesar);
 //arreglo para guardar los nombres de todas las listas desplegables
 var nombresListasDesplegables = [];
 
 
+var totalDatoLeido18=0;//variable para la cantidad de elementos del json
+//recuperamos del localStorage el valor del total del json
+totalDatoLeido18= localStorage.totalElementosJson;
      //Creamos tantas posiciones en el arreglo como total de listas desplegables hay
      //iniciamos el contador en 1 ya que no existe la variable que inicie en cero  ver script.js 852
      for(var lst=1; lst<=totalListasDesplegablesProcesar;lst++){
         nombresListasDesplegables[lst] = document.getElementById('lista'+lst) ;
       }
+
+      //añadimos opción vacia a la lista desplegable auxiliar
          var tipoCancer = new Object();
-         tipoCancer.valor="1";
-         tipoCancer.nombre="posicion69";
-     
+         tipoCancer.valor="0";
+         tipoCancer.nombre=" ";
+
      
      var options18="";
      options18 += '<option value="' +tipoCancer.valor +'">' +tipoCancer.nombre +'</option>';
      nombresListasDesplegables[1].innerHTML = options18;
 
 
-     
+     miListaTipoCancerMenor18.min=1; //agregamos el valor mínimo a la caja de tipo number
+   
+
+     //validamos si dejan vacio el campo
+        if ( miListaTipoCancerMenor18.value.length == 0 ||  miListaTipoCancerMenor18.value  =='' ||  miListaTipoCancerMenor18.value ==null || /^\\s+$/.test( miListaTipoCancerMenor18.value )) {
+            alert('Diligencie Datos.No se pueden dejar campos vacios en tipo cáncer.');
+            miListaTipoCancerMenor18.focus();
+            miListaTipoCancerMenor18.value='Diligencie datos';
+        }//fin if
+        else{
+//EL CAMPO NO ESTÁ VACIO, PROCEDEMOS A LEER EL JSON DE LA LISTA DESPLEGABLE
+      
+            
 //PROCESO PARA LEER EL JSON QUE TIENE LOS TIPÓS DE CANCER
 var misCabeceras = new Headers();
 
@@ -102,15 +123,19 @@ var miInit = { method: 'GET',
                         //si no especificamos el objeto, el json solamente tiene un elemento, es decir, solamente tiene el objeto tipoCancer
                     datoLeido18=Object.keys(myJson18.tipoCancer).length ;
                         //luego de obtener la respuesta  con el contenido del json, lo mostramos en consola. Podemos mostrarlo sin ningún tratamiento ya que la consola permite ver objetos
-                        console.log(myJson18);
+                    console.log("eL JSON obtenido fue "+myJson18);
                     // console.log('La cantidad de elementos que tiene el json en el objeto "personas" es : '+datoLeido);    ---DESCOMENTARIAR SI SE USA EL JSON PERSONAS
                     console.log('La cantidad de elementos que tiene el json en el objeto "tipoCancer" es : '+datoLeido18);
-                    alert("EL json obtenido fue: ");
                     //stringify convierte el objeto en json para ser visualizado por el alert, o si no  solo se observa object object
-                    alert("EL json obtenido fue: "+JSON.stringify(myJson18));
+                    //alert("EL json obtenido fue: "+JSON.stringify(myJson18));
                     // alert('La cantidad de elementos que tiene el json en el objeto "personas" es : '+datoLeido);    --DESCOMENTARIAR SI SE USA EL JSON PERSONAS
-                    alert('La cantidad de elementos que tiene el json en el objeto "tipoCancer" es : '+datoLeido18);
+                   // alert('La cantidad de elementos que tiene el json en el objeto "tipoCancer" es : '+datoLeido18);
 
+                    //la cantidad de elementos del json será el atributo máximo para la caja tipo number
+                    //con la cantidad de elementos que tiene el json limitamos la cantidad de datos que se pueden ingresar en la caja tipo number
+                    miListaTipoCancerMenor18.max = datoLeido18;
+                    //guardamos en la variable web storage el valor que desaparecerá
+                    localStorage.totalElementosJson=datoLeido18;
                             //recorremos el json
             for(var i18=0; i18<datoLeido18;i18++){
                         
@@ -119,26 +144,59 @@ var miInit = { method: 'GET',
                 nombresListasDesplegables[1].innerHTML = options18;
                  }//fin for
 
+
+
                 
                     })//fin  //then(function(myJson18)
                     .catch(function(error18) {
                         console.log('Hubo un problema con la petición Fetch:' + error18.message);
                         console.log("La cabecera solicitada de contenido fue: "+myContentType);
                       });
+
+
+                      //DESPUES DE OBTGENER LOS DATOS Y SABER QUE NO GENERÓ ERROR, VALIDAMOS SI LO QUE SE ESCRIBA EN LA CAJA NUMBER ESTÁ DENTRO DEL RANGO
+                     //convierto a entero los datos tipo texto para validacion
+    if((miListaTipoCancerMenor18.value<1) || (miListaTipoCancerMenor18.value>totalDatoLeido18)){
+        alert('Digite un valor entre 1 y '+ totalDatoLeido18);
+        var mensajeAlerta="";
+        mensajeAlerta+="1: Leucemia Linfoide Aguda.\n 2: Leucemia Mieloide Aguda.\n 3: Otras Leucemias.\n 4: Linfomas y Neoplasias Reticuloendoteliales.\n 5: Tumores del Sistema Nervioso Central.\n";
+        mensajeAlerta+="6: Neuroblastoma y otros tumores de celulas nerviosas periféricas.\n 7: Retinoblastoma.\n 8: Tumores Renales.\n 9: Tumores Hepáticos.\n 10: Tumores Óseos Malignos.\n";
+        mensajeAlerta+="11: Sarcomas de Tejidos Blandos y Extraóseos.\n 12: Tumores Germinales, Trofoblásticos, y otros Gonadales.\n 13: Tumores Epiteliales Malignos y Melanoma.\n 14: Otras Neoplasias Malignas No Especificadas.\n";
+        alert(mensajeAlerta);
+
+
+        miListaTipoCancerMenor18.focus();// no podemos dejar el foco en el campo porque volveria 
+        miListaTipoCancerMenor18.value=1;
+    }//fin if
                     
-                    
 
 
 
 
+            
+
+
+
+        }//fin else vacio
+
+
+
+
+ 
+                
 
 
 
       } else {
         // Sorry! No Web Storage support..
         alert("Lo siento. Su navegador no soporta Web Storage. Intente con otro navegador");
-      }
+      }//else web storage
 
 
 
 }
+/*
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+*/
